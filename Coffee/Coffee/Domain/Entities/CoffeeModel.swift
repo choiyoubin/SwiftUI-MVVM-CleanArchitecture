e5caea124f2f392e7f38c89f733cc4b2c17e7385
ChoiYouBin <cub8533@naver.com>
2024-12-24 02:03:22 +0900

    [Feat] 초기 네이밍, 폴더링 설계

---
+++ Coffee/Coffee/Domain/Entities/CoffeeModel.swift
@@ -0,0 +1,7 @@
+//
+//  CoffeeModel.swift
+//  Coffee
+//
+//  Created by 최유빈 on 12/24/24.
+//
+
---
+++ Coffee/Coffee/Domain/UseCases/FetchSortedCoffeeUseCase.swift
@@ -0,0 +1,7 @@
+//
+//  FetchSortedCoffeeUseCase.swift
+//  Coffee
+//
+//  Created by 최유빈 on 12/24/24.
+//
+
