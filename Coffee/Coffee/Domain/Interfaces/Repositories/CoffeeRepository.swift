9dc155969ed3501bce154d747b3412fb945944ce
ChoiYouBin <cub8533@naver.com>
2024-12-28 04:17:10 +0900

    Delete File

--- Coffee/Coffee/Domain/Interfaces/Repositories/CoffeeRepository.swift
+++ Coffee/Coffee/Domain/Interfaces/Repositories/CoffeeRepository.swift
@@ -1,12 +1,1 @@
-//
-//  CoffeRepository.swift
-//  Coffee
-//
-//  Created by 최유빈 on 12/24/24.
-//
 
-import Foundation
-
-protocol CoffeeRepository {
-    func fetchCoffeeList(completion: @escaping ([CoffeeDTO.CoffeeData]) -> Void)
-}
