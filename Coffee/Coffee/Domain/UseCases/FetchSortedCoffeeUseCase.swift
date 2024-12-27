7f15d3abce2a815d07a1fd27ab0d0004440dacda
ChoiYouBin <cub8533@naver.com>
2024-12-27 17:52:54 +0900

    [Feat] Study Code 작성

--- Coffee/Coffee/Domain/UseCases/FetchSortedCoffeeUseCase.swift
+++ Coffee/Coffee/Domain/UseCases/FetchSortedCoffeeUseCase.swift
@@ -5,3 +5,23 @@
 //  Created by 최유빈 on 12/24/24.
 //
 
+protocol FetchSortedCoffeeUseCase {
+    func execute(completion: @escaping([CoffeeDTO.CoffeeData]) -> Void)
+}
+
+class DefaultFetchSortedCoffeeUseCase: FetchSortedCoffeeUseCase {
+    private let coffeeRepository: CoffeeRepository
+    
+    init(repository: CoffeeRepository) {
+        self.coffeeRepository = repository
+    }
+    
+    func execute(completion: @escaping ([CoffeeDTO.CoffeeData]) -> Void) {
+        coffeeRepository.fetchCoffeeList { coffeeList in
+            let latteList = coffeeList.filter { coffee in
+                coffee.title.lowercased().contains("latte")
+            }
+            completion(latteList)
+        }
+    }
+}
