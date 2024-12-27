b5992a19ee1e824e65c7095f449463c04a643565
ChoiYouBin <cub8533@naver.com>
2024-12-28 04:21:01 +0900

    [Feat] code 재작성

--- Coffee/Coffee/Domain/Entities/CoffeeModel.swift
+++ Coffee/Coffee/Domain/Entities/CoffeeModel.swift
@@ -1,1 +1,19 @@
+//
+//  CoffeeModel.swift
+//  Coffee
+//
+//  Created by 최유빈 on 12/24/24
+//
+
+struct CoffeeList {
+    let coffeeData: [Coffee]
+}
+
+struct Coffee {
+    let title: String
+    let description: String
+    let ingredients: [String]
+    let image: String
+    let id: Int
+}
 
--- Coffee/Coffee/Presentatiton/Main/ViewModel/MainViewModel.swift
+++ Coffee/Coffee/Presentatiton/Main/ViewModel/MainViewModel.swift
@@ -1,1 +1,24 @@
+//
+//  MainViewModel.swift
+//  Coffee
+//
+//  Created by 최유빈 on 12/24/24.
+//
 
+import SwiftUI
+
+class MainViewModel: ObservableObject {
+    @Published var coffeeList: [CoffeeDTO.CoffeeData] = []
+    private let fetchSortedCoffeeUseCase: FetchSortedCoffeeUseCase
+    
+    init(fetchSortedCoffeeUseCase: FetchSortedCoffeeUseCase) {
+        self.fetchSortedCoffeeUseCase = fetchSortedCoffeeUseCase
+    }
+    
+    func fetchLatteList() {
+        fetchSortedCoffeeUseCase.execute { [weak self] coffeeList in
+            guard let self = self else { return }
+            self.coffeeList = coffeeList
+        }
+    }
+}
