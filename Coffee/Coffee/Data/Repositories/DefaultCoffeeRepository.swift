cad764d700768456c0503b5034aef37a51014736
ChoiYouBin <cub8533@naver.com>
2025-01-02 02:27:46 +0900

    [Feat] DefaultCoffeeRepository 수정

--- Coffee/Coffee/Data/Repositories/DefaultCoffeeRepository.swift
+++ Coffee/Coffee/Data/Repositories/DefaultCoffeeRepository.swift
@@ -10,19 +10,15 @@ import Alamofire
 
 class DefaultCoffeeRepository: CoffeeRepository {
     
-    @Published var coffeeList: [CoffeeDTO.CoffeeData] = []
-    
     private let baseURL = "https://api.sampleapis.com/coffee/hot"
     
     func fetchCoffeeList(completion: @escaping ([CoffeeDTO.CoffeeData]) -> Void) {
         AF.request(baseURL)
             .validate()
-            .responseDecodable(of: [CoffeeDTO.CoffeeData].self) { [weak self] response in
-                guard let self = self else { return }
+            .responseDecodable(of: [CoffeeDTO.CoffeeData].self) { response in
                 
                 switch response.result {
                 case .success(let coffeeData):
-                    self.coffeeList = coffeeData
                     completion(coffeeData)
                 case .failure(let error):
                     print("\(error.localizedDescription)")
